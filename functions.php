0b802178b5ab314c8c295c8f6f3991a4c22d9cfb
Nick Whitmoyer <nick@whitmoyer.com>
2012-03-24 13:08:21 -0400

    Performance, security, and appearance
    Default for image links will be set to none, image src paths will be
    relative with no height & width attributes and empty alt tags will be
    replaced with title text. Login error messages will be obscured,
    theme/plugin editor will not be available from admin, and call for
    Google CDN version of jQuery.

--- functions.php
+++ functions.php
@@ -14,4 +14,47 @@ if ( function_exists('register_sidebar') )
 		'after_title' => '',
 	));
 
+
+//Changes default for image link URLs to none
+update_option('image_default_link_type','none');
+
+
+// Removes the domain name from the src URL, the height and width attributes and replaces empty alt attributes with the title text on images
+function image_tag($html, $id, $alt, $title) {
+	return preg_replace(array(
+			'/'.str_replace('//','\/\/',get_bloginfo('url')).'/i',
+			'/\s+width="\d+"/i',
+			'/\s+height="\d+"/i',
+			'/alt=""/i'
+		),
+		array(
+			'',
+			'',
+			'',
+			'alt="' . $title . '"'
+		),
+		$html);
+}
+add_filter('get_image_tag', 'image_tag', 0, 4);
+
+
+// Obscure login screen error messages
+function wpfme_login_obscure(){ return '<strong>Sorry</strong>: Think you have gone wrong somwhere!';}
+add_filter( 'login_errors', 'wpfme_login_obscure' );
+
+
+// Disable the theme / plugin text editor in Admin
+define('DISALLOW_FILE_EDIT', true);
+
+
+// Call the google CDN version of jQuery for the frontend
+// Make sure you use this with wp_enqueue_script('jquery'); in your header
+function wpfme_jquery_enqueue() {
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
+	wp_enqueue_script('jquery');
+}
+if (!is_admin()) add_action("wp_enqueue_scripts", "wpfme_jquery_enqueue", 11);
+
 ?>
+
